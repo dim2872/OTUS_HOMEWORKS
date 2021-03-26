1d5c4aadea16f45335ef28bcd3f9cb3098a7b897
Dmitriy F <dim2872@yandex.ru>
2021-03-26 19:49:00 +0300

    delete double of func. and change func. places

--- homework_01/main.py
+++ homework_01/main.py
@@ -13,29 +13,27 @@ def power_numbers(data):
     """
     return [number ** 2 for number in data]
 
+
 # filter types
 ODD = "odd"
 EVEN = "even"
 PRIME = "prime"
 
 
-def filter_numbers(data, flag):
-    """
-    функция, которая на вход принимает список из целых чисел,
-    и возвращает только чётные/нечётные/простые числа
-    (выбор производится передачей дополнительного аргумента)
+def prime_numbers(data):
+    lst = []
+    for i in range(len(data)):
+        for a in range(2, data[i]):
+            if (data[i] % a) == 0:
+                break
+            elif (data[i] // a) == 1:
+                lst.append(data[i])
+                break
+    return lst
 
-    >>> filter_numbers([1, 2, 3], ODD)
-    <<< [1, 3]
-    >>> filter_numbers([2, 3, 4, 5], EVEN)
-    <<< [2, 4]
-    """
-ODD = "odd"
-EVEN = "even"
-PRIME = "prime"
 
 def filter_numbers(data, flag):
-        """
+    """
         функция, которая на вход принимает список из целых чисел,
         и возвращает только чётные/нечётные/простые числа
         (выбор производится передачей дополнительного аргумента)
@@ -45,20 +43,9 @@ def filter_numbers(data, flag):
         #>>> filter_numbers([2, 3, 4, 5], EVEN)
         <<< [2, 4]
         """
-        if flag == ODD:
-            return [number for number in data if number % 2 != 0]
-        elif flag == EVEN:
-            return [number for number in data if number % 2 == 0]
-        elif flag == PRIME:
-            return prime_numbers(data)
-
-    def prime_numbers(data):
-        lst = []
-        for i in range(len(data)):
-            for a in range(2, data[i]):
-                if (data[i] % a) == 0:
-                    break
-                elif (data[i] // a) == 1:
-                    lst.append(data[i])
-                    break
-        return lst
+    if flag == ODD:
+        return [number for number in data if number % 2 != 0]
+    elif flag == EVEN:
+        return [number for number in data if number % 2 == 0]
+    elif flag == PRIME:
+        return prime_numbers(data)
