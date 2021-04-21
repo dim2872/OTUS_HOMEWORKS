dd20604756860e9ce2cf62dc6d1f03ed68893ddd
Dmitriy F <dim2872@yandex.ru>
2021-04-21 21:50:17 +0300

    TypeError: __init__() takes from 1 to 4 positional arguments but 5 were given

--- homework_02/base.py
+++ homework_02/base.py
@@ -3,13 +3,14 @@ from homework_02 import exceptions
 
 
 class Vehicle(ABC):
+    started = False
 
-    def __init__(self, weight=0.0, started=False, fuel=0.0, fuel_consumption=0.0):
+    def __init__(self, weight=0, fuel=0, fuel_consumption=0):
         self.fuel = fuel
         self.weight = weight
         self.fuel_consumption = fuel_consumption
 
-    def start(self, started):
+    def start(self, started=False):
         if started:
             raise exceptions.LowFuelError
         else:
--- homework_02/plane.py
+++ homework_02/plane.py
@@ -6,10 +6,10 @@ from homework_02 import exceptions
 
 
 class Plane(Vehicle):
-    def __init__(self, cargo, max_cargo, weight, fuel, fuel_consumption):
-        self.max_cargo = max_cargo
-        self.cargo = cargo
+    def __init__(self, weight, fuel, fuel_consumption, max_cargo):
         super().__init__(self, weight, fuel, fuel_consumption)
+        self.max_cargo = max_cargo
+        self.cargo = 0
 
     def load_cargo(self, add_cargo):
         if self.max_cargo <= self.cargo + add_cargo:
@@ -17,7 +17,7 @@ class Plane(Vehicle):
         else:
             raise exceptions.CargoOverload
 
-    def remove_all_cargo(self, add_cargo):
+    def remove_all_cargo(self):
         cargo_before = self.cargo
-        self.cargo = 0.0
+        self.cargo = 0
         return cargo_before
