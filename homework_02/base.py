3c1f81273bafdd24ed882970dc355baaef9169e3
Dmitriy F <dim2872@yandex.ru>
2021-05-01 22:57:06 +0300

    last edition base.py

--- homework_02/base.py
+++ homework_02/base.py
@@ -4,18 +4,21 @@ from homework_02 import exceptions
 
 class Vehicle(ABC):
     started = False
+    weight = 0
+    fuel = 0
+    fuel_consumption = 0
 
-    def __init__(self, weight=0, fuel=0, fuel_consumption=0):
-        self.fuel = fuel
+    def __init__(self, weight, fuel, fuel_consumption):
         self.weight = weight
+        self.fuel = fuel
         self.fuel_consumption = fuel_consumption
 
-    def start(self, started=False):
-        if started:
-            raise exceptions.LowFuelError
-        else:
+    def start(self, started, fuel):
+        if started == False and fuel > 0:
             started = True
-        return started
+            return started
+        else:
+            raise exceptions.LowFuelError
 
     def move(self, fuel, distance, fuel_consumption):
         if fuel < fuel_consumption * distance:
